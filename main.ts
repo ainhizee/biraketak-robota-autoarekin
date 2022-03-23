038ea77fef4439b11f4e906ab933512830e7055d
ainhizee <93262250+ainhizee@users.noreply.github.com>
2022-03-23 14:01:33 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -9,6 +9,7 @@ input.onButtonPressed(Button.A, function () {
     DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 50)
     DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CCW, 50)
     basic.pause(2000)
+    DFRobotMaqueenPlus.mototStop(Motors.ALL)
     basic.showLeds(`
         . . # . .
         . # . . .
@@ -19,5 +20,6 @@ input.onButtonPressed(Button.A, function () {
     DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 50)
     DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CCW, 50)
     basic.pause(2000)
+    DFRobotMaqueenPlus.mototStop(Motors.ALL)
 })
 DFRobotMaqueenPlus.I2CInit()
